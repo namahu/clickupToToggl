51d12aabc7483e3b9da3127bb79aefe0651ea0f9
Masami_Nakaoka <neti0326@gmail.com>
2019-10-23 17:58:09 +0000

    feat: Create doPost function

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,37 @@
 function doPost(e) {
-    console.log(e);
 
-    const params = JSON.parse(e.postData.getDataAsString());
+    const params: any = JSON.parse(e.postData.getDataAsString());
+
+    if (params.event !== 'taskUpdated') {
+        throw new Error('Invalid event.');
+    }
+
+    const historyItem = params.history_items[0];
+
+    if (historyItem.field !== 'status') {
+        throw new Error('Invalid field');
+    }
+
+    const taskId: string = params.task_id;
+    const historyItemAfter = historyItem.after;
+
+    console.log('ここまで来た');
+
+    switch (historyItemAfter.status) {
+        case 'in progress':
+            startTimeTrack(taskId);
+            break;
+        case 'Closed':
+            stopTimeTrack(taskId);
+            break;
+        case 'pause':
+            stopTimeTrack(taskId);
+            break;
+        default:
+    }
     console.log(params);
+
+    return ContentService
+        .createTextOutput(JSON.stringify({ 'statusCode': 200 }))
+        .setMimeType(ContentService.MimeType.JSON);
 };
