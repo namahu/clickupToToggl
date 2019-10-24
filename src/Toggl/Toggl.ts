3a79e27c6425ce56c51cb6d10bebcf75db193034
Masami_Nakaoka <neti0326@gmail.com>
2019-10-24 18:19:32 +0000

    feat: creating startTimeEntry function #1hnnr3[in progress]

--- src/Toggl/Toggl.ts
+++ src/Toggl/Toggl.ts
@@ -1,32 +1,14 @@
-type method = 'get' | 'post' | 'put';
-
-interface Options {
-    method?: method;
-    headers: {
-        'Content-Type': string;
-        'Authorization': string;
-    };
-    payload?: string;
-    muteHttpExceptions: boolean;
-}
-
-
 class Toggl {
-    apiToken:  string;
-    baseURL: string;
+
+    private apiToken: string;
 
     constructor(apiToken: string) {
         this.apiToken = apiToken;
-        this.baseURL = 'https://www.toggl.com/api/v8/';
     }
 
-    createEndPoint = (path: string): string => {
-        return `${this.baseURL}${path}`;
-    }
-
-    startTimeEntry = () => {
-        const endPoint: string = this.createEndPoint('time_entries/start');
-
+    startTimeEntry = (obj) => {
+        const req = new TogglRequest_(this.apiToken);
+        return req.post('time_entries/start', obj);
     }
 
     stopTimeEntry = (timeEntryId: number) => {
