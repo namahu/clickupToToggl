09786272d450526b2916efb587b418faccd1a5b6
namahu <neti0326@gmail.com>
2019-10-25 19:59:15 +0900

    feat: Create time entry functions #1hnnr3[completed]

--- src/Toggl/Toggl.ts
+++ src/Toggl/Toggl.ts
@@ -1,4 +1,5 @@
 interface TimeEntries {
+    id?: number;
     description: string;
     wid: number;
     pid?: number;
@@ -13,6 +14,10 @@ interface TimeEntries {
     at?: string;
 };
 
+interface ResponseTimeEntries {
+    data: TimeEntries;
+}
+
 class Toggl {
 
     private apiToken: string;
@@ -21,17 +26,31 @@ class Toggl {
         this.apiToken = apiToken;
     }
 
+    private getTimeEntryId_(obj: ResponseTimeEntries) {
+        return obj.data.id;
+
+    }
+
+    private getRunningTimeEntry_(req): ResponseTimeEntries {
+        return req.get(`time_entries/current`);
+    }
+
     startTimeEntry = (obj: TimeEntries) => {
         const req = new TogglRequest_(this.apiToken);
         return req.post('time_entries/start', obj);
     }
 
-    stopTimeEntry = (timeEntryId: number) => {
+    stopTimeEntry = () => {
+        const req = new TogglRequest_(this.apiToken);
+        const runningTimeEntry = this.getRunningTimeEntry_(req);
+        const timeEntryId: number = this.getTimeEntryId_(runningTimeEntry);
+        return req.put(`time_entries/${timeEntryId}/stop`);
 
     }
 
     getRunningTimeEntry = () => {
-
+        const req = new TogglRequest_(this.apiToken);
+        return this.getRunningTimeEntry_(req);
     }
 
 };
