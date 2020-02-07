c11098e5af272ed9434295631c01cfaf36aa230a
namahu <neti0326@gmail.com>
2020-02-07 10:46:38 +0900

    計測開始するステータス名を変更

--- src/main.ts
+++ src/main.ts
@@ -6,80 +6,81 @@
  *
  */
 const createTogglTimeEntry = (task, projects) => {
+  const getTagDataInTask = (tags): string[] => {
+    if (tags.length === 0) {
+      return [];
+    }
+    return tags.map(tag => tag.name);
+  };
 
-    const getTagDataInTask = (tags): string[] => {
-        if (tags.length === 0) {
-            return [];
-        }
-        return tags.map(tag => tag.name);
-    };
+  const getTogglProjectIdFromClickupTask = (list, projects) => {
+    return projects.filter(project => project[1] === list.name);
+  };
 
-    const getTogglProjectIdFromClickupTask = (list, projects) => {
-        return projects.filter(project => project[1] === list.name);
-    };
-
-    return {
-        description: task.name,
-        tags: getTagDataInTask(task.tags),
-        pid: getTogglProjectIdFromClickupTask(task.list, projects)[0][0],
-        created_with: 'ClickupToToggl'
-    };
+  return {
+    description: task.name,
+    tags: getTagDataInTask(task.tags),
+    pid: getTogglProjectIdFromClickupTask(task.list, projects)[0][0],
+    created_with: "ClickupToToggl"
+  };
 };
 
-const getTogglAllProjectIdAndName = (toggl) => {
-    const togglCurrentUserData = toggl.getCurrentUser(true);
-    const projects = togglCurrentUserData.data.projects;
+const getTogglAllProjectIdAndName = toggl => {
+  const togglCurrentUserData = toggl.getCurrentUser(true);
+  const projects = togglCurrentUserData.data.projects;
 
-    return projects.map(pj => [pj.id, pj.name]);
+  return projects.map(pj => [pj.id, pj.name]);
 };
 
 function doPost(e) {
+  const params: any = JSON.parse(e.postData.getDataAsString());
+  console.log(params);
 
-    const params: any = JSON.parse(e.postData.getDataAsString());
-    console.log(params);
+  if (params.event !== "taskUpdated") {
+    throw new Error("Invalid event.");
+  }
 
-    if (params.event !== 'taskUpdated') {
-        throw new Error('Invalid event.');
-    }
+  const historyItem = params.history_items[0];
 
-    const historyItem = params.history_items[0];
+  if (historyItem.field !== "status") {
+    throw new Error("Invalid field");
+  }
 
-    if (historyItem.field !== 'status') {
-        throw new Error('Invalid field');
-    }
+  const taskId: string = params.task_id;
+  const historyItemAfter = historyItem.after;
 
-    const taskId: string = params.task_id;
-    const historyItemAfter = historyItem.after;
+  const scriptProperties = PropertiesService.getScriptProperties().getProperties();
+  const togglApiToken: string = scriptProperties.TOGGL_API_TOKEN;
+  const clickupApiToken: string = scriptProperties.CLICKUP_API_TOKEN;
 
-    const scriptProperties = PropertiesService.getScriptProperties().getProperties();
-    const togglApiToken: string = scriptProperties.TOGGL_API_TOKEN;
-    const clickupApiToken: string = scriptProperties.CLICKUP_API_TOKEN;
+  const toggl: Toggl = Toggl.getToggl(togglApiToken);
 
-    const toggl: Toggl = Toggl.getToggl(togglApiToken);
+  let res;
+  switch (historyItemAfter.status) {
+    case "tracking start":
+      const clickup: Clickup = Clickup.getClickup(clickupApiToken);
 
-    let res;
-    switch (historyItemAfter.status) {
-        case 'in progress':
-            const clickup: Clickup = Clickup.getClickup(clickupApiToken);
+      const clickupTask = clickup.getTaskByTaskId(taskId);
+      const togglAllProjects = getTogglAllProjectIdAndName(toggl);
+      const togglTimeEntry = createTogglTimeEntry(
+        clickupTask,
+        togglAllProjects
+      );
 
-            const clickupTask = clickup.getTaskByTaskId(taskId);
-            const togglAllProjects = getTogglAllProjectIdAndName(toggl);
-            const togglTimeEntry = createTogglTimeEntry(clickupTask, togglAllProjects);
+      res = startTimeTrack(toggl, togglTimeEntry);
 
-            res = startTimeTrack(toggl, togglTimeEntry);
+      break;
+    case "Closed":
+      res = stopTimeTrack(toggl);
+      break;
+    case "pause":
+      res = stopTimeTrack(toggl);
 
-            break;
-        case 'Closed':
-            res = stopTimeTrack(toggl);
-            break;
-        case 'pause':
-            res = stopTimeTrack(toggl);
+      break;
+    default:
+  }
 
-            break;
-        default:
-    }
-
-    return ContentService
-        .createTextOutput(JSON.stringify({ 'statusCode': 200 }))
-        .setMimeType(ContentService.MimeType.JSON);
-};
+  return ContentService.createTextOutput(
+    JSON.stringify({ statusCode: 200 })
+  ).setMimeType(ContentService.MimeType.JSON);
+}
